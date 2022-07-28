JamesAHenderson/go-opera-debug_p2p

add support of multidb config

cmd/opera/launcher/config.go
@@ -135,7 +135,7 @@ type config struct {
 	Lachesis      abft.Config
 	LachesisStore abft.StoreConfig
 	VectorClock   vecmt.IndexConfig
-	cachescale    cachescale.Func
+	DBs           integration.DBsConfig
 }
 
 func (c *config) AppConfigs() integration.Configs {
@@ -145,6 +145,7 @@ func (c *config) AppConfigs() integration.Configs {
 		Lachesis:      c.Lachesis,
 		LachesisStore: c.LachesisStore,
 		VectorClock:   c.VectorClock,
+		DBs:           c.DBs,
 	}
 }
 
@@ -373,7 +374,6 @@ func mayMakeAllConfigs(ctx *cli.Context) (*config, error) {
 		Lachesis:      abft.DefaultConfig(),
 		LachesisStore: abft.DefaultStoreConfig(cacheRatio),
 		VectorClock:   vecmt.DefaultConfig(cacheRatio),
-		cachescale:    cacheRatio,
 	}
 
 	if ctx.GlobalIsSet(FakeNetFlag.Name) {
@@ -388,6 +388,14 @@ func mayMakeAllConfigs(ctx *cli.Context) (*config, error) {
 		if err := loadAllConfigs(file, &cfg); err != nil {
 			return &cfg, err
 		}
+	}
+	// apply default for DB config if it wasn't touched by config file
+	dbDefault := integration.DefaultDBsConfig(cacheRatio.U64)
+	if len(cfg.DBs.Routing.Table) == 0 {
+		cfg.DBs.Routing = dbDefault.Routing
+	}
+	if len(cfg.DBs.Cache.Table) == 0 {
+		cfg.DBs.Cache = dbDefault.Cache
 	}
 
 	// Apply flags (high priority)

hadv/go-opera

cmd/opera/launcher/config.go
@@ -148,7 +148,7 @@ type config struct {
 	Lachesis      abft.Config
 	LachesisStore abft.StoreConfig
 	VectorClock   vecmt.IndexConfig
-	cachescale    cachescale.Func
+	DBs           integration.DBsConfig
 }
 
 func (c *config) AppConfigs() integration.Configs {
@@ -158,6 +158,7 @@ func (c *config) AppConfigs() integration.Configs {
 		Lachesis:      c.Lachesis,
 		LachesisStore: c.LachesisStore,
 		VectorClock:   c.VectorClock,
+		DBs:           c.DBs,
 	}
 }
 
@@ -369,7 +370,6 @@ func mayMakeAllConfigs(ctx *cli.Context) (*config, error) {
 		Lachesis:      abft.DefaultConfig(),
 		LachesisStore: abft.DefaultStoreConfig(cacheRatio),
 		VectorClock:   vecmt.DefaultConfig(cacheRatio),
-		cachescale:    cacheRatio,
 	}
 
 	if ctx.GlobalIsSet(FakeNetFlag.Name) {
@@ -387,6 +387,17 @@ func mayMakeAllConfigs(ctx *cli.Context) (*config, error) {
 		if err := loadAllConfigs(file, &cfg); err != nil {
 			return &cfg, err
 		}
+	}
+	// apply default for DB config if it wasn't touched by config file
+	dbDefault := integration.DefaultDBsConfig(cacheRatio.U64, uint64(utils.MakeDatabaseHandles()))
+	if len(cfg.DBs.Routing.Table) == 0 {
+		cfg.DBs.Routing = dbDefault.Routing
+	}
+	if len(cfg.DBs.GenesisCache.Table) == 0 {
+		cfg.DBs.GenesisCache = dbDefault.GenesisCache
+	}
+	if len(cfg.DBs.RuntimeCache.Table) == 0 {
+		cfg.DBs.RuntimeCache = dbDefault.RuntimeCache
 	}
 
 	// Apply flags (high priority)